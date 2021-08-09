bc3480ad7dad77b8c63118d809266424f09c5106
Gerasim Gerasimov <dialix@yandex.ru>
2021-08-10 00:41:57 +0700

    добавил путь до сервисов

--- services-install.sh
+++ services-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "1) Install SerialPortsService"
+cd /usr/DExS/services
 sudo git clone https://github.com/GerasimGerasimov/SerialportsService.git
 cd SerialportsService
 sudo npm install
