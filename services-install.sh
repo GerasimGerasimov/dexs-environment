3cca11050dc9b3a474dae9e2f8c569030306f0e0
Gerasim Gerasimov <dialix@yandex.ru>
2021-08-10 00:43:03 +0700

    добавил выход из установленного сервиса

--- services-install.sh
+++ services-install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "1) Install SerialPortsService"
 cd /usr/DExS/services
+echo "1) Install SerialPortsService"
 sudo git clone https://github.com/GerasimGerasimov/SerialportsService.git
 cd SerialportsService
 sudo npm install
 sudo npm run build
+cd ..
